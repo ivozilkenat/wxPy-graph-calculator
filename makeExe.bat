344aecf6206722cdfc1cf022315aab9d89d4a7a8
hoischreck <44005173+hoischreck@users.noreply.github.com>
2021-09-03 15:10:20 +0200

    Update makeExe.bat

--- makeExe.bat
+++ makeExe.bat
@@ -1,5 +1,9 @@
 @echo off
+:: [---------Below you have to specifiy the project directory-------------]
+
 set project_dir_path = "C:\Users\hoisc\PycharmProjects\Grafikrechner"
+
+::[-----------------------------------------------------------------------]
 cd %project_dir_path%
 pyinstaller --windowed --onefile main.py
 
